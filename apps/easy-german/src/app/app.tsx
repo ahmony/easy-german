8a1784f5aa78f46df230374776ec67f5586fe555
Ahmic <aahmic@dccsintra.net>
2023-10-30 13:57:23 +0100

    Initial commit

---
+++ apps/easy-german/src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="easy-german" />
+    </div>
+  );
+}
+
+export default App;
